2724af4b4ff65e0f70eba1fa9553deba706f53ce
zhenhai <weizhenhai@netbank.cn>
2023-11-12 11:15:34 +0800

    #

--- apis/recorder/v1/types.go
+++ apis/recorder/v1/types.go
@@ -13,8 +13,9 @@ type Recorder struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   RecorderSpec `json:"spec"`
-	Status StatusSpec
+	Spec         RecorderSpec     `json:"spec"`
+	Cmd          CmdSpec          //主动下发的
+	RunningState RunningStateSpec //主动上报的
 }
 
 // RecorderSpec is the spec for a Recorder resource
@@ -23,9 +24,16 @@ type RecorderSpec struct {
 	Vendor string
 }
 
-type StatusSpec struct {
-	Updated string
-	Val     string //deleting,deleted,creating,created,pausing,paused
+type CmdSpec struct {
+	Created  metav1.Time
+	Updated  metav1.Time
+	Val      string //deleting,deleted,creating,created,pausing,paused
+	Feedback string //ok,otherwise error string
+}
+
+type RunningStateSpec struct {
+	Updated metav1.Time
+	Val     string //work, pause, rest
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
